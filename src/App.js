4c15b8bd3c1b5fa385ac4c3c75bee7d2430911a4
Attila Fekete <feketeattila777@proton.me>
2024-09-24 20:09:12 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <span>
+          <img src="darts-b&w.jpg" alt="bullseye"></img>
+        </span>
+        <h1>AIM TRACKER</h1>
+        <span>
+          <img src="darts-b&w.jpg" alt="bullseye"></img>
+        </span>
       </header>
+      <form>
+        <span>
+          <p>Add Your Aim for</p>
+        </span>
+        <span>
+          <select>
+            <option value="today">Today</option>
+            <option value="week">This week</option>
+            <option value="month">This month</option>
+            <option value="year">This year</option>
+          </select>
+        </span>
+        <span>
+          <p>to achieve:</p>
+        </span>
+        <span>
+          {
+            //make it dinamically scale its width with input length (?)
+          }
+          <input type="text" placeholder="Type your task here" />
+        </span>
+        <button>Add it!</button>
+      </form>
+      <div className="aim-list">
+        <div className="aim-today">
+          <h3>Today</h3>
+        </div>
+        <div className="aim-week">
+          <h3>This week</h3>
+        </div>
+        <div className="aim-month">
+          <h3>This month</h3>
+        </div>
+        <div className="aim-year">
+          <h3>This year</h3>
+        </div>
+      </div>
+      <div className="sort">
+        <p>Sort my aims by</p>
+        <select>
+          <option>Order of input</option>
+          <option>Alphabet</option>
+          <option>Importance</option>
+        </select>
+        <button>Sort it!</button>
+      </div>
     </div>
   );
 }
